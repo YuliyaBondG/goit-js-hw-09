6ab08ba89ed1e77fb60daf912ac8edb6b5fb7290
YuliyaBondG <manyunka@ukr.net>
2023-03-03 14:13:31 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+import '../css/common.css';
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+stopBtn.disabled = true;
+let timerId = null;
+startBtn.addEventListener('click', onBtnStart);
+
+function onBtnStart() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+stopBtn.addEventListener('click', onBtnStop);
+
+function onBtnStop() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
