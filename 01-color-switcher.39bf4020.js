3ae37807089b07e1b65af88316a4d5551a3f3ccc
YuliyaBondG <113913438+YuliyaBondG@users.noreply.github.com>
2023-03-03 12:14:12 +0000

    Deploying to gh-pages from @ YuliyaBondG/goit-js-hw-09@6ab08ba89ed1e77fb60daf912ac8edb6b5fb7290 🚀

---
+++ 01-color-switcher.39bf4020.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("body"),t=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");a.disabled=!0;var d=null;t.addEventListener("click",(function(){d=setInterval((function(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,a.disabled=!1})),a.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,a.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.39bf4020.js.map
